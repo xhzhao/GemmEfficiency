25ba379a4629bf5071d41cc8035f351a6545e25d
xhzhao <xiaohui.zhao@intel.com>
2017-07-04 21:21:28 +0800

    Update run_gemm.sh

--- sgemm_peek/run_gemm.sh
+++ sgemm_peek/run_gemm.sh
@@ -3,7 +3,7 @@ export MKL_ENABLE_INSTRUCTIONS=AVX512_MIC_E1
 export OMP_NUM_THREADS=72
 
 #MKL IGEMM
-numactl -m 1 ./test.x 50000 50000 8640 NN 1 1 0 0 0 0 10
+numactl -m 0 ./test.x 50000 50000 8640 NN 1 1 0 0 0 0 10
 #MKL SGEMM
-numactl -m 1 ./stest.x 50000 50000 4320 NN 1 1 10
+numactl -m 0 ./stest.x 50000 50000 4320 NN 1 1 10
 
