79ecdde858f3912c36aacae223f7094197101587
GenPeiForIntelCorp <gen.pei@intel.com>
2017-07-06 21:58:32 +0800

    Performance improved 4x~5x
    -bash-4.2$ ./test.bin
    main start
    ----------GEMM 11----------
    sgemm_mkl end, avg time = 0.01, GFLOPS = 3.00
    sgemm_profile end, avg time = 0.00, GFLOPS = 12.35
    verify result OK.
    ----------GEMM 11----------
    sgemm_mkl end, avg time = 0.01, GFLOPS = 2.48
    sgemm_profile end, avg time = 0.00, GFLOPS = 11.57
    verify result OK.
    ----------GEMM 11----------
    sgemm_mkl end, avg time = 0.01, GFLOPS = 2.74
    sgemm_profile end, avg time = 0.00, GFLOPS = 11.53
    verify result OK.
    ----------GEMM 11----------
    sgemm_mkl end, avg time = 0.01, GFLOPS = 2.98
    sgemm_profile end, avg time = 0.00, GFLOPS = 10.64
    verify result OK.
    ----------GEMM 11----------
    sgemm_mkl end, avg time = 0.01, GFLOPS = 3.69
    sgemm_profile end, avg time = 0.00, GFLOPS = 12.54
    verify result OK.
    ----------GEMM 11----------
    sgemm_mkl end, avg time = 0.00, GFLOPS = 2.65
    sgemm_profile end, avg time = 0.00, GFLOPS = 10.63
    verify result OK.
    ----------GEMM 11----------
    sgemm_mkl end, avg time = 0.01, GFLOPS = 2.63
    sgemm_profile end, avg time = 0.00, GFLOPS = 10.81
    verify result OK.
    ----------GEMM 11----------
    sgemm_mkl end, avg time = 0.01, GFLOPS = 3.41
    sgemm_profile end, avg time = 0.00, GFLOPS = 12.41
    verify result OK.
    ----------GEMM 11----------
    sgemm_mkl end, avg time = 0.01, GFLOPS = 4.61
    sgemm_profile end, avg time = 0.00, GFLOPS = 12.12
    verify result OK.
    ----------GEMM 11----------
    sgemm_mkl end, avg time = 0.01, GFLOPS = 3.83
    sgemm_profile end, avg time = 0.00, GFLOPS = 12.60
    verify result OK.
    ----------GEMM 11----------
    sgemm_mkl end, avg time = 0.01, GFLOPS = 5.78
    sgemm_profile end, avg time = 0.00, GFLOPS = 13.49
    verify result OK.
    ----------GEMM 11----------
    sgemm_mkl end, avg time = 0.01, GFLOPS = 3.97
    sgemm_profile end, avg time = 0.00, GFLOPS = 12.73
    verify result OK.
    ----------GEMM 11----------
    sgemm_mkl end, avg time = 0.01, GFLOPS = 4.50
    sgemm_profile end, avg time = 0.00, GFLOPS = 12.84
    verify result OK.
    ----------GEMM 11----------
    sgemm_mkl end, avg time = 0.01, GFLOPS = 4.13
    sgemm_profile end, avg time = 0.00, GFLOPS = 12.48
    verify result OK.
    ----------GEMM 11----------
    sgemm_mkl end, avg time = 0.01, GFLOPS = 3.33
    sgemm_profile end, avg time = 0.00, GFLOPS = 12.93
    verify result OK.
    ----------GEMM 11----------
    sgemm_mkl end, avg time = 0.01, GFLOPS = 3.72
    sgemm_profile end, avg time = 0.00, GFLOPS = 12.47
    verify result OK.
    ----------GEMM 11----------
    sgemm_mkl end, avg time = 0.01, GFLOPS = 3.06
    sgemm_profile end, avg time = 0.00, GFLOPS = 11.69
    verify result OK.
    ----------GEMM 11----------
    sgemm_mkl end, avg time = 0.01, GFLOPS = 3.19
    sgemm_profile end, avg time = 0.00, GFLOPS = 12.12
    verify result OK.
    ----------GEMM 11----------
    sgemm_mkl end, avg time = 0.01, GFLOPS = 3.18
    sgemm_profile end, avg time = 0.00, GFLOPS = 11.08
    verify result OK.
    ----------GEMM 11----------
    sgemm_mkl end, avg time = 0.00, GFLOPS = 2.24
    sgemm_profile end, avg time = 0.00, GFLOPS = 11.11
    verify result OK.
    ----------GEMM 11----------
    sgemm_mkl end, avg time = 0.01, GFLOPS = 2.61
    sgemm_profile end, avg time = 0.00, GFLOPS = 11.45
    verify result OK.
    ----------GEMM 11----------
    sgemm_mkl end, avg time = 0.01, GFLOPS = 3.84
    sgemm_profile end, avg time = 0.00, GFLOPS = 13.43
    verify result OK.
    ----------GEMM 11----------
    sgemm_mkl end, avg time = 0.01, GFLOPS = 2.21
    sgemm_profile end, avg time = 0.00, GFLOPS = 11.70
    verify result OK.
    ----------GEMM 11----------
    sgemm_mkl end, avg time = 0.00, GFLOPS = 2.78
    sgemm_profile end, avg time = 0.00, GFLOPS = 12.91
    verify result OK.
    ----------GEMM 11----------
    sgemm_mkl end, avg time = 0.01, GFLOPS = 2.58
    sgemm_profile end, avg time = 0.00, GFLOPS = 11.61
    verify result OK.
    -bash-4.2$

--- gemm11.c
+++ gemm11.c
@@ -15,122 +15,110 @@ sgemm_opt(uint64_t m, uint64_t n, const uint64_t p,
 {
   n = 500;
 
-#define TNUM (1)
+  for (uint64_t j = 0; j < n; j += 16) {
+    __mmask16 mask = 0xffff;
 
-//#pragma omp parallel num_threads(TNUM)
-  {
-
-    int tid = omp_get_thread_num();
-
-    uint64_t block_id_j = tid;
-    uint64_t thread_size_j = n;
-
-    for (uint64_t tid_j = 0; tid_j < thread_size_j; tid_j += 16) {
-      uint64_t j = thread_size_j * block_id_j + tid_j;
-      __mmask16 mask = 0xffff;
-
-      uint64_t k = 0;
-      if ((j + 16) >= n) {
-        mask = (1u << (n - j)) - 1;
-      }
-
-      __m512 zero = _mm512_setzero_ps();
-
-      __m512 c0_0_i_0_f_j_0 = _mm512_setzero_ps();
-      __m512 c0_0_i_0_f_j_1 = _mm512_setzero_ps();
-      __m512 c0_0_i_0_f_j_2 = _mm512_setzero_ps();
-      __m512 c0_0_i_0_f_j_3 = _mm512_setzero_ps();
-      __m512 c0_0_i_0_f_j   = _mm512_setzero_ps();
-
-      for (; k + 16 <= p; k += 16) {
-        float *a0_f_i_0_f_k_base = (float *)(A + k);
-        float *b0_f_k_0_f_j_base = (float *)(B + (k + 0x0) * n + j);
-
-
-        __m512 b0_0_k_0_f_j = _mm512_mask_load_ps(zero, mask, b0_f_k_0_f_j_base + n * 0x0);
-        __m512 b1_1_k_0_f_j = _mm512_mask_load_ps(zero, mask, b0_f_k_0_f_j_base + n * 0x1);
-        __m512 b2_2_k_0_f_j = _mm512_mask_load_ps(zero, mask, b0_f_k_0_f_j_base + n * 0x2);
-        __m512 b3_3_k_0_f_j = _mm512_mask_load_ps(zero, mask, b0_f_k_0_f_j_base + n * 0x3);
-        __m512 b4_4_k_0_f_j = _mm512_mask_load_ps(zero, mask, b0_f_k_0_f_j_base + n * 0x4);
-        __m512 b5_5_k_0_f_j = _mm512_mask_load_ps(zero, mask, b0_f_k_0_f_j_base + n * 0x5);
-        __m512 b6_6_k_0_f_j = _mm512_mask_load_ps(zero, mask, b0_f_k_0_f_j_base + n * 0x6);
-        __m512 b7_7_k_0_f_j = _mm512_mask_load_ps(zero, mask, b0_f_k_0_f_j_base + n * 0x7);
-        __m512 b8_8_k_0_f_j = _mm512_mask_load_ps(zero, mask, b0_f_k_0_f_j_base + n * 0x8);
-        __m512 b9_9_k_0_f_j = _mm512_mask_load_ps(zero, mask, b0_f_k_0_f_j_base + n * 0x9);
-        __m512 ba_a_k_0_f_j = _mm512_mask_load_ps(zero, mask, b0_f_k_0_f_j_base + n * 0xa);
-        __m512 bb_b_k_0_f_j = _mm512_mask_load_ps(zero, mask, b0_f_k_0_f_j_base + n * 0xb);
-        __m512 bc_c_k_0_f_j = _mm512_mask_load_ps(zero, mask, b0_f_k_0_f_j_base + n * 0xc);
-        __m512 bd_d_k_0_f_j = _mm512_mask_load_ps(zero, mask, b0_f_k_0_f_j_base + n * 0xd);
-        __m512 be_e_k_0_f_j = _mm512_mask_load_ps(zero, mask, b0_f_k_0_f_j_base + n * 0xe);
-        __m512 bf_f_k_0_f_j = _mm512_mask_load_ps(zero, mask, b0_f_k_0_f_j_base + n * 0xf);
-
-        __m128 *a0_0_i_0_f_k = (__m128 *)(a0_f_i_0_f_k_base + p * 0x0);
-
-        c0_0_i_0_f_j_0 = _mm512_4fmadd_ps(c0_0_i_0_f_j_0, b0_0_k_0_f_j, b1_1_k_0_f_j, b2_2_k_0_f_j, b3_3_k_0_f_j, a0_0_i_0_f_k + 0x0);
-        c0_0_i_0_f_j_1 = _mm512_4fmadd_ps(c0_0_i_0_f_j_1, b4_4_k_0_f_j, b5_5_k_0_f_j, b6_6_k_0_f_j, b7_7_k_0_f_j, a0_0_i_0_f_k + 0x1);
-        c0_0_i_0_f_j_2 = _mm512_4fmadd_ps(c0_0_i_0_f_j_2, b8_8_k_0_f_j, b9_9_k_0_f_j, ba_a_k_0_f_j, bb_b_k_0_f_j, a0_0_i_0_f_k + 0x2);
-        c0_0_i_0_f_j_3 = _mm512_4fmadd_ps(c0_0_i_0_f_j_3, bc_c_k_0_f_j, bd_d_k_0_f_j, be_e_k_0_f_j, bf_f_k_0_f_j, a0_0_i_0_f_k + 0x3);
-      }
-
-      if (k + 8 <= p) {
-        float *a0_f_i_0_7_k_base = (float *)(A + k);
-        float *b0_7_k_0_f_j_base = (float *)(B + (k + 0x0) * n + j);
-
-
-        __m512 b0_0_k_0_f_j = _mm512_mask_load_ps(zero, mask, b0_7_k_0_f_j_base + n * 0x0);
-        __m512 b1_1_k_0_f_j = _mm512_mask_load_ps(zero, mask, b0_7_k_0_f_j_base + n * 0x1);
-        __m512 b2_2_k_0_f_j = _mm512_mask_load_ps(zero, mask, b0_7_k_0_f_j_base + n * 0x2);
-        __m512 b3_3_k_0_f_j = _mm512_mask_load_ps(zero, mask, b0_7_k_0_f_j_base + n * 0x3);
-        __m512 b4_4_k_0_f_j = _mm512_mask_load_ps(zero, mask, b0_7_k_0_f_j_base + n * 0x4);
-        __m512 b5_5_k_0_f_j = _mm512_mask_load_ps(zero, mask, b0_7_k_0_f_j_base + n * 0x5);
-        __m512 b6_6_k_0_f_j = _mm512_mask_load_ps(zero, mask, b0_7_k_0_f_j_base + n * 0x6);
-        __m512 b7_7_k_0_f_j = _mm512_mask_load_ps(zero, mask, b0_7_k_0_f_j_base + n * 0x7);
-
-        __m128 *a0_0_i_0_7_k = (__m128 *)(a0_f_i_0_7_k_base + p * 0x0);
-
-        c0_0_i_0_f_j_0 = _mm512_4fmadd_ps(c0_0_i_0_f_j_0, b0_0_k_0_f_j, b1_1_k_0_f_j, b2_2_k_0_f_j, b3_3_k_0_f_j, a0_0_i_0_7_k + 0x0);
-        c0_0_i_0_f_j_1 = _mm512_4fmadd_ps(c0_0_i_0_f_j_1, b4_4_k_0_f_j, b5_5_k_0_f_j, b6_6_k_0_f_j, b7_7_k_0_f_j, a0_0_i_0_7_k + 0x1);
-        
-        k += 8;
-      }
-
-      if (k + 4 <= p) {
-        float *a0_3_i_0_f_k_base = (float *)(A + k);
-        float *b0_3_k_0_f_j_base = (float *)(B + (k + 0x0) * n + j);
-
-        __m512 b0_0_k_0_f_j = _mm512_mask_load_ps(zero, mask, b0_3_k_0_f_j_base + n * 0x0);
-        __m512 b1_1_k_0_f_j = _mm512_mask_load_ps(zero, mask, b0_3_k_0_f_j_base + n * 0x1);
-        __m512 b2_2_k_0_f_j = _mm512_mask_load_ps(zero, mask, b0_3_k_0_f_j_base + n * 0x2);
-        __m512 b3_3_k_0_f_j = _mm512_mask_load_ps(zero, mask, b0_3_k_0_f_j_base + n * 0x3);
-
-        __m128 *a0_0_i_0_3_k = (__m128 *)(a0_3_i_0_f_k_base);
-
-        c0_0_i_0_f_j_0 = _mm512_4fmadd_ps(c0_0_i_0_f_j_0, b0_0_k_0_f_j, b1_1_k_0_f_j, b2_2_k_0_f_j, b3_3_k_0_f_j, a0_0_i_0_3_k + 0x0);
-        
-        k += 4;
-      }
-
-      for (; k < p; ++k) {
-        __m512 b_k_0_f_j = _mm512_mask_load_ps(zero, mask, B + (k + 0x0) * n + j);
-        __m512 a_i_0_f_k = _mm512_set1_ps (*(A + k));
-
-        c0_0_i_0_f_j_0 = _mm512_fmadd_ps(b_k_0_f_j, a_i_0_f_k, c0_0_i_0_f_j_0);
-      }
-
-      c0_0_i_0_f_j_0 = _mm512_add_ps(c0_0_i_0_f_j_0, c0_0_i_0_f_j_1);
-      c0_0_i_0_f_j_2 = _mm512_add_ps(c0_0_i_0_f_j_2, c0_0_i_0_f_j_3);
-      c0_0_i_0_f_j = _mm512_add_ps(c0_0_i_0_f_j_2, c0_0_i_0_f_j_0);
-      _mm512_mask_store_ps(C + j, mask, c0_0_i_0_f_j);
-
+    uint64_t k = 0;
+    if ((j + 16) >= n) {
+    mask = (1u << (n - j)) - 1;
     }
+
+    __m512 zero = _mm512_setzero_ps();
+
+    __m512 c0_0_i_0_f_j_0 = _mm512_setzero_ps();
+    __m512 c0_0_i_0_f_j_1 = _mm512_setzero_ps();
+    __m512 c0_0_i_0_f_j_2 = _mm512_setzero_ps();
+    __m512 c0_0_i_0_f_j_3 = _mm512_setzero_ps();
+    __m512 c0_0_i_0_f_j   = _mm512_setzero_ps();
+
+    for (; k + 16 <= p; k += 16) {
+      float *a0_f_i_0_f_k_base = (float *)(A + k);
+      float *b0_f_k_0_f_j_base = (float *)(B + (k + 0x0) * n + j);
+
+
+      __m512 b0_0_k_0_f_j = _mm512_mask_load_ps(zero, mask, b0_f_k_0_f_j_base + n * 0x0);
+      __m512 b1_1_k_0_f_j = _mm512_mask_load_ps(zero, mask, b0_f_k_0_f_j_base + n * 0x1);
+      __m512 b2_2_k_0_f_j = _mm512_mask_load_ps(zero, mask, b0_f_k_0_f_j_base + n * 0x2);
+      __m512 b3_3_k_0_f_j = _mm512_mask_load_ps(zero, mask, b0_f_k_0_f_j_base + n * 0x3);
+      __m512 b4_4_k_0_f_j = _mm512_mask_load_ps(zero, mask, b0_f_k_0_f_j_base + n * 0x4);
+      __m512 b5_5_k_0_f_j = _mm512_mask_load_ps(zero, mask, b0_f_k_0_f_j_base + n * 0x5);
+      __m512 b6_6_k_0_f_j = _mm512_mask_load_ps(zero, mask, b0_f_k_0_f_j_base + n * 0x6);
+      __m512 b7_7_k_0_f_j = _mm512_mask_load_ps(zero, mask, b0_f_k_0_f_j_base + n * 0x7);
+      __m512 b8_8_k_0_f_j = _mm512_mask_load_ps(zero, mask, b0_f_k_0_f_j_base + n * 0x8);
+      __m512 b9_9_k_0_f_j = _mm512_mask_load_ps(zero, mask, b0_f_k_0_f_j_base + n * 0x9);
+      __m512 ba_a_k_0_f_j = _mm512_mask_load_ps(zero, mask, b0_f_k_0_f_j_base + n * 0xa);
+      __m512 bb_b_k_0_f_j = _mm512_mask_load_ps(zero, mask, b0_f_k_0_f_j_base + n * 0xb);
+      __m512 bc_c_k_0_f_j = _mm512_mask_load_ps(zero, mask, b0_f_k_0_f_j_base + n * 0xc);
+      __m512 bd_d_k_0_f_j = _mm512_mask_load_ps(zero, mask, b0_f_k_0_f_j_base + n * 0xd);
+      __m512 be_e_k_0_f_j = _mm512_mask_load_ps(zero, mask, b0_f_k_0_f_j_base + n * 0xe);
+      __m512 bf_f_k_0_f_j = _mm512_mask_load_ps(zero, mask, b0_f_k_0_f_j_base + n * 0xf);
+
+      __m128 *a0_0_i_0_f_k = (__m128 *)(a0_f_i_0_f_k_base + p * 0x0);
+
+      c0_0_i_0_f_j_0 = _mm512_4fmadd_ps(c0_0_i_0_f_j_0, b0_0_k_0_f_j, b1_1_k_0_f_j, b2_2_k_0_f_j, b3_3_k_0_f_j, a0_0_i_0_f_k + 0x0);
+      c0_0_i_0_f_j_1 = _mm512_4fmadd_ps(c0_0_i_0_f_j_1, b4_4_k_0_f_j, b5_5_k_0_f_j, b6_6_k_0_f_j, b7_7_k_0_f_j, a0_0_i_0_f_k + 0x1);
+      c0_0_i_0_f_j_2 = _mm512_4fmadd_ps(c0_0_i_0_f_j_2, b8_8_k_0_f_j, b9_9_k_0_f_j, ba_a_k_0_f_j, bb_b_k_0_f_j, a0_0_i_0_f_k + 0x2);
+      c0_0_i_0_f_j_3 = _mm512_4fmadd_ps(c0_0_i_0_f_j_3, bc_c_k_0_f_j, bd_d_k_0_f_j, be_e_k_0_f_j, bf_f_k_0_f_j, a0_0_i_0_f_k + 0x3);
+    }
+
+    if (k + 8 <= p) {
+      float *a0_f_i_0_7_k_base = (float *)(A + k);
+      float *b0_7_k_0_f_j_base = (float *)(B + (k + 0x0) * n + j);
+
+
+      __m512 b0_0_k_0_f_j = _mm512_mask_load_ps(zero, mask, b0_7_k_0_f_j_base + n * 0x0);
+      __m512 b1_1_k_0_f_j = _mm512_mask_load_ps(zero, mask, b0_7_k_0_f_j_base + n * 0x1);
+      __m512 b2_2_k_0_f_j = _mm512_mask_load_ps(zero, mask, b0_7_k_0_f_j_base + n * 0x2);
+      __m512 b3_3_k_0_f_j = _mm512_mask_load_ps(zero, mask, b0_7_k_0_f_j_base + n * 0x3);
+      __m512 b4_4_k_0_f_j = _mm512_mask_load_ps(zero, mask, b0_7_k_0_f_j_base + n * 0x4);
+      __m512 b5_5_k_0_f_j = _mm512_mask_load_ps(zero, mask, b0_7_k_0_f_j_base + n * 0x5);
+      __m512 b6_6_k_0_f_j = _mm512_mask_load_ps(zero, mask, b0_7_k_0_f_j_base + n * 0x6);
+      __m512 b7_7_k_0_f_j = _mm512_mask_load_ps(zero, mask, b0_7_k_0_f_j_base + n * 0x7);
+
+      __m128 *a0_0_i_0_7_k = (__m128 *)(a0_f_i_0_7_k_base + p * 0x0);
+
+      c0_0_i_0_f_j_0 = _mm512_4fmadd_ps(c0_0_i_0_f_j_0, b0_0_k_0_f_j, b1_1_k_0_f_j, b2_2_k_0_f_j, b3_3_k_0_f_j, a0_0_i_0_7_k + 0x0);
+      c0_0_i_0_f_j_1 = _mm512_4fmadd_ps(c0_0_i_0_f_j_1, b4_4_k_0_f_j, b5_5_k_0_f_j, b6_6_k_0_f_j, b7_7_k_0_f_j, a0_0_i_0_7_k + 0x1);
+      
+      k += 8;
+    }
+
+    if (k + 4 <= p) {
+      float *a0_3_i_0_f_k_base = (float *)(A + k);
+      float *b0_3_k_0_f_j_base = (float *)(B + (k + 0x0) * n + j);
+
+      __m512 b0_0_k_0_f_j = _mm512_mask_load_ps(zero, mask, b0_3_k_0_f_j_base + n * 0x0);
+      __m512 b1_1_k_0_f_j = _mm512_mask_load_ps(zero, mask, b0_3_k_0_f_j_base + n * 0x1);
+      __m512 b2_2_k_0_f_j = _mm512_mask_load_ps(zero, mask, b0_3_k_0_f_j_base + n * 0x2);
+      __m512 b3_3_k_0_f_j = _mm512_mask_load_ps(zero, mask, b0_3_k_0_f_j_base + n * 0x3);
+
+      __m128 *a0_0_i_0_3_k = (__m128 *)(a0_3_i_0_f_k_base);
+
+      c0_0_i_0_f_j_0 = _mm512_4fmadd_ps(c0_0_i_0_f_j_0, b0_0_k_0_f_j, b1_1_k_0_f_j, b2_2_k_0_f_j, b3_3_k_0_f_j, a0_0_i_0_3_k + 0x0);
+      
+      k += 4;
+    }
+
+    for (; k < p; ++k) {
+      __m512 b_k_0_f_j = _mm512_mask_load_ps(zero, mask, B + (k + 0x0) * n + j);
+      __m512 a_i_0_f_k = _mm512_set1_ps (*(A + k));
+
+      c0_0_i_0_f_j_0 = _mm512_fmadd_ps(b_k_0_f_j, a_i_0_f_k, c0_0_i_0_f_j_0);
+    }
+
+    c0_0_i_0_f_j_0 = _mm512_add_ps(c0_0_i_0_f_j_0, c0_0_i_0_f_j_1);
+    c0_0_i_0_f_j_2 = _mm512_add_ps(c0_0_i_0_f_j_2, c0_0_i_0_f_j_3);
+    c0_0_i_0_f_j = _mm512_add_ps(c0_0_i_0_f_j_2, c0_0_i_0_f_j_0);
+    _mm512_mask_store_ps(C + j, mask, c0_0_i_0_f_j);
+
   }
 }
 
 void sgemm11_opt(char* pTransA, char* pTransB,
-      const int* pM, const int* pN, const int* pK,
-      const float *pAlpha, const float *pa, const int*plda,
-      const float *pb, const int *pldb, const float *pBeta,
-      float *pc, const int*pldc)
+    const int* pM, const int* pN, const int* pK,
+    const float *pAlpha, const float *pa, const int*plda,
+    const float *pb, const int *pldb, const float *pBeta,
+    float *pc, const int*pldc)
 {
   const uint64_t m = *pN;
   const uint64_t n = *pM;
