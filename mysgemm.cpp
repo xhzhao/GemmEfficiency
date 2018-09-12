d5436a959ba93c0af70d6deb83c06cacade197a1
xhzhao <xiaohui.zhao@intel.com>
2018-09-12 10:41:53 +0800

    add v2 blocking, achieve 1393 GFLOPS

--- mysgemm.cpp
+++ mysgemm.cpp
@@ -51,8 +51,55 @@ int reducesum_and_set(float* dst, __m512 s0, __m512 s1, __m512 s2, __m512 s3,
 #define NB 16
 #define KB 16
 #define KCB 256
+//calc [5,16] of C
+//get a col of A to vector, get a row of B to vector
+//get a col of C ,scatter to memory finally
+void gemmblock_5_16_v2(int M, int N, int K,int kmin, int kmax, int m, int n, const float * A, const int lda, const float * B, const int ldb, float * C, const int ldc) {
+
+  float* cbase = C + m * N + n;
+
+  __m512 c0x = _mm512_load_ps(cbase + 0x00 * N);
+  __m512 c1x = _mm512_load_ps(cbase + 0x01 * N);
+  __m512 c2x = _mm512_load_ps(cbase + 0x02 * N);
+  __m512 c3x = _mm512_load_ps(cbase + 0x03 * N);
+  __m512 c4x = _mm512_load_ps(cbase + 0x04 * N);
+
+
+  for(int k = kmin; k < kmax; k++) {
+    //load a col of A to a vector
+	const float* abase = A + m * K + k;
+	const float* bbase = B + k * N + n;
+
+    //load [16,1] of A to 16  vector
+    __m512 a00 = _mm512_set1_ps(*(abase + 0x00 * K));
+    __m512 a10 = _mm512_set1_ps(*(abase + 0x01 * K));
+    __m512 a20 = _mm512_set1_ps(*(abase + 0x02 * K));
+    __m512 a30 = _mm512_set1_ps(*(abase + 0x03 * K));
+    __m512 a40 = _mm512_set1_ps(*(abase + 0x04 * K));
+
+    
+    __m512 b0x = _mm512_load_ps(bbase);
+    
+    c0x = _mm512_fmadd_ps(a00, b0x, c0x);
+    c1x = _mm512_fmadd_ps(a10, b0x, c1x);
+    c2x = _mm512_fmadd_ps(a20, b0x, c2x);
+    c3x = _mm512_fmadd_ps(a30, b0x, c3x);
+    c4x = _mm512_fmadd_ps(a40, b0x, c4x);
+  }
+  
+  //store [16,16] result to C
+  _mm512_store_ps(cbase + 0x00 * N, c0x);
+  _mm512_store_ps(cbase + 0x01 * N, c1x);
+  _mm512_store_ps(cbase + 0x02 * N, c2x);
+  _mm512_store_ps(cbase + 0x03 * N, c3x);
+  _mm512_store_ps(cbase + 0x04 * N, c4x);
+  
+}
+
+
 
 //calc [5,16] of C
+//get a row of A to vector, get a col of B to vector, reduce vector to a float finally 
 void gemmblock_5_16(int M, int N, int K,int kmin, int kmax, int m, int n, const float * A, const int lda, const float * B, const int ldb, float * C, const int ldc) {
 
     int BL = N * 4;
@@ -311,14 +358,15 @@ int my_sgemm_opt4(char transa, char transb,
     //printf("tid = %d, [m = %d:%d, n = %d:%d] \n", tid, m_start, m_end, n_start, n_end);
 
     //for(int kc = 0; kc < K; kc+=KCB) {
-    //  int kmax = (K) < (kc+KCB) ? (K) : (kc+KCB);
+      //int kmax = (K) < (kc+KCB) ? (K) : (kc+KCB);
       for(int m = m_start; m < m_end; m += MB) {
         for(int n = n_start; n < n_end; n += NB) {
 
           //this block will calc [5,16] of C
           //gemmblock_5_16(M, N, K, kc, kmax, m, n, A, lda, B, ldb, C, ldc);
-          gemmblock_5_16(M, N, K, 0, K, m, n, A, lda, B, ldb, C, ldc);
-
+          //gemmblock_5_16(M, N, K, 0, K, m, n, A, lda, B, ldb, C, ldc);
+          gemmblock_5_16_v2(M, N, K, 0, K, m, n, A, lda, B, ldb, C, ldc);
+          //gemmblock_5_16_v2(M, N, K, kc, kmax, m, n, A, lda, B, ldb, C, ldc);
         }
       }
     //}
